05ee8e791aab5bee352413a132b248ab0ddc9e5d
Vadym Vatsenko <vadim.vatsenko@gmail.com>
2024-03-16 22:11:03 +0200

    start List HomeWork

---
+++ HomeWork_4/HomeWork_04_01_List/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//1. Создать список целых чисел. Добавьте в этот список 10 рандомных чисел с помощью цикла и функции Add.
+//Выведите максимальный и минимальный элемент списка и их индексы OK
+
+namespace HomeWork_04_01_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            List<int> numb = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                numb.Add(rnd.Next(1, 20));
+            }
+
+            int MinValue = 0;
+            int MaxValue = 0;
+
+            for (int i = 0; i < numb.Count; i++)
+            {
+                
+            }
+        }
+    }
+}
