58d64121ef315923cc288035f2855836bd4f641a
Vadym Vatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2024-06-07 13:10:12 +0300

    finish asyn awat

--- HomeWork_20/HomeWork_20_2_Async_Await/Circle2.cs
+++ HomeWork_20/HomeWork_20_2_Async_Await/Circle2.cs
@@ -2,68 +2,76 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HomeWork_20_2_Async_Await
 {
-    public class Circle : Shape
+    public class Circle2: Shape
     {
-        private int radius;
-
-        public Circle(Vector2 position, ConsoleColor color, char symbol, int radius) : base(position, color, symbol)
+        public int radius { get; private set; }
+        public Circle2(Vector2 position, ConsoleColor color, char symbol, int radius) : base(position, color, symbol)
         {
             this.radius = radius;
         }
 
-        async public override Task Draw()
+        public async IAsyncEnumerable<Vector2> CircleDrawAsync()
         {
-            for (double angle = 0; angle < 359; angle++)
+
+            Console.ForegroundColor = color;
+
+            
+            for (double angle = 0; angle < 360; angle += 0.5)
             {
-                Console.ForegroundColor = color;
                 double X = position.x + Math.Cos(angle) * radius;
                 double Y = position.y + Math.Sin(angle) * radius;
                 Console.SetCursorPosition((int)X, (int)Y);
-                await Task.Delay(50);
                 Console.Write(symbol);
-
-                await Task.Run(() => FillDraw(X, Y));
+                await Task.Delay(1500);
+                yield return new Vector2(X,Y);
             }
+
+        }
+
+
+
+        public override Task Draw()
+        {
+            throw new NotImplementedException();
         }
 
         async public Task FillDraw(double x, double y)
         {
-            Circle fillCircle = new Circle(new Vector2(x + 50, y), ConsoleColor.Magenta, '&', 2);
+            //Circle fillCircle = new Circle(new Vector2(x + 50, y), ConsoleColor.Magenta, '&', 2);
 
             Console.ForegroundColor = color;
 
-            int startX = (int)fillCircle.position.x;
-            int startY = (int)fillCircle.position.y;
+            int startX = (int)position.x;
+            int startY = (int)position.y;
 
             Console.SetCursorPosition(startX, startY);
 
-            double step = 1.0 / fillCircle.radius;
+            double step = 1.0 / radius;
 
             for (double i = 0; i <= Math.PI / 2; i += step)
             {
-                int X = (int)Math.Round(fillCircle.radius * Math.Cos(i));
-                int Y = (int)Math.Round(fillCircle.radius * Math.Sin(i));
+                int X = (int)Math.Round(radius * Math.Cos(i));
+                int Y = (int)Math.Round(radius * Math.Sin(i));
 
 
                 for (int j = startY - Y; j <= startY + Y; j++)
                 {
                     Console.SetCursorPosition(startX - X, j);
-                    Console.Write(fillCircle.symbol);
+                    Console.Write(symbol);
                     Console.SetCursorPosition(startX + X, j);
-                    Console.Write(fillCircle.symbol);
+                    Console.Write(symbol);
                 }
 
-                await Task.Delay(50);
+                await Task.Delay(1500);
                 Console.SetCursorPosition(startX, startY + Y);
-                Console.Write(fillCircle.symbol);
+                Console.Write(symbol);
                 Console.SetCursorPosition(startX, startY - Y);
-                Console.Write(fillCircle.symbol);
+                Console.Write(symbol);
             }
         }
     }
--- HomeWork_20/HomeWork_20_2_Async_Await/Program.cs
+++ HomeWork_20/HomeWork_20_2_Async_Await/Program.cs
@@ -11,14 +11,27 @@ namespace HomeWork_20_2_Async_Await // Note: actual namespace depends on the pro
             Console.WindowWidth = 120;
             Console.BufferHeight = 70;
             Console.WindowHeight = 70;
-            Circle circle =  new Circle(new Vector2(20, 35), ConsoleColor.Red, '#', 15);
+            /*Circle circle =  new Circle(new Vector2(20, 35), ConsoleColor.Red, '#', 15);
+
+            
 
             await Task.Run(() => circle.Draw());
 
             Console.SetCursorPosition(0, (int)(circle.position.y + 20f));
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Program end");
+            Console.WriteLine("Program end");*/
 
+            Circle2 mainCircle = new Circle2(new Vector2(20, 35), ConsoleColor.Red, '#', 15);
+
+            //await Task.Run(() => mainCircle.CircleDrawAsync());
+
+            await foreach(var c in mainCircle.CircleDrawAsync())
+            {
+                Circle2 secondCircle = new Circle2(new Vector2(c.x + 40, c.y), ConsoleColor.Magenta, '&', 2);
+
+                await secondCircle.FillDraw(secondCircle.position.x, secondCircle.position.y) ;
+            }
+           
         }
     }
 }
